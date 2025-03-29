d0113ea39d009b8251a819991ac8f7a16aea41b7
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-03-29 14:36:35 +0700

    SCF run

--- debug_Al_fcc_01.py
+++ debug_Al_fcc_01.py
@@ -36,4 +36,37 @@ from my_gpaw_initialize import my_gpaw_initialize
 #calc.initialize(atoms)
 my_gpaw_initialize(calc, atoms)
 
+# Need this for projections (?)
+calc.set_positions(atoms)
+
+#
+# from scf.irun
+#
+calc.scf.eigensolver_used = getattr(calc.wfs.eigensolver, "name", None)
+calc.scf.check_eigensolver_state(calc.wfs, calc.hamiltonian, calc.density)
+calc.scf.niter = 1
+converged = False
+
+while calc.scf.niter <= calc.scf.maxiter:
+    calc.scf.iterate_eigensolver(calc.wfs, calc.hamiltonian, calc.density)
+
+    calc.scf.check_convergence(
+        calc.density, calc.hamiltonian, calc.wfs, calc.log, calc.call_observers)
+
+    converged = (calc.scf.converged and
+                    calc.scf.niter >= calc.scf.niter_fixdensity)
+    if converged:
+        calc.scf.do_if_converged(calc.wfs, calc.hamiltonian, calc.density, calc.log)
+        break
+
+    calc.scf.update_ham_and_dens(calc.wfs, calc.hamiltonian, calc.density)
+    calc.scf.niter += 1
+
+# Don't fix the density in the next step.
+calc.scf.niter_fixdensity = 0
+
+if not converged:
+    calc.scf.not_converged(calc.dens, calc.ham, calc.wfs, calc.log)
+
+
 print("\n---- Script ended normally ----")
